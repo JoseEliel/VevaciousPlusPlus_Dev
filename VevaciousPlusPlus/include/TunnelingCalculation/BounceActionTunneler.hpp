e491a7ea6fcc21c5ae237c6e92d4aa4db8c882ab
José Eliel Camargo-Molina <>
2020-05-25 16:07:30 +0200

    Typo.

--- VevaciousPlusPlus/include/TunnelingCalculation/BounceActionTunneler.hpp
+++ VevaciousPlusPlus/include/TunnelingCalculation/BounceActionTunneler.hpp
@@ -176,7 +176,7 @@ namespace VevaciousPlusPlus
     // the field origin. 
 
     bool DsbRolledToOrigin( falseVacuum.LengthSquared()
-                                      < thresholdSeparationSquared )
+                                      < thresholdSeparationSquared );
 
     if(DsbRolledToOrigin)
         {
